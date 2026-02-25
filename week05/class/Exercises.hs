b2802027a1fdddf8d277d1d5eddb14f7cd851d3a
theKevKev <kevinhan.119@gmail.com>
2026-02-24 20:44:35 -0500

    exercises week 5

--- week05/class/Exercises.hs
+++ week05/class/Exercises.hs
@@ -5,13 +5,15 @@ module Exercises where
 data Stream a = Cons a (Stream a)
 
 streamTake :: Int -> Stream a -> [a]
-streamTake = error "unimplemented"
+streamTake i (Cons x xs)
+  | i <= 0 = []
+  | otherwise = x : streamTake (i - 1) xs
 
 {- Make a Stream from a list. If the list is finite, all remaining elements
     have the default value.-}
 
 streamRepeat :: a -> Stream a
-streamRepeat = error "unimplemented"
+streamRepeat x = Cons x (streamRepeat x)
 
 streamFromList :: a -> [a] -> Stream a
-streamFromList = error "unimplemented"
+streamFromList x = foldr Cons (streamRepeat x)
