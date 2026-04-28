6b6c4d422b4216d50da950d4936c57162668a861
theKevKev <kevinhan.119@gmail.com>
2026-04-27 22:31:38 -0400

    homework 13

--- week13/homework/Exercises.hs
+++ week13/homework/Exercises.hs
@@ -34,6 +34,24 @@ arguments to StringVal to those containing only characters satisfying
 shrink as well.
 -}
 
+instance Arbitrary Value where
+  arbitrary :: Gen Value
+  arbitrary = oneof [genNilVal, genIntVal, genBoolVal, genStringVal]
+    where
+      genNilVal = return NilVal
+
+      genIntVal = fmap IntVal arbitrary
+
+      genBoolVal = fmap BoolVal arbitrary
+
+      genStringVal = fmap StringVal $ listOf (suchThat arbitrary isAllowedChar)
+
+  shrink :: Value -> [Value]
+  shrink NilVal = []
+  shrink (IntVal x) = map IntVal (shrink x)
+  shrink (BoolVal x) = map BoolVal (shrink x)
+  shrink (StringVal x) = map StringVal (filter (all isAllowedChar) (shrink x))
+
 isAllowedChar :: Char -> Bool
 isAllowedChar c = isDigit c || isAsciiLower c || isAsciiUpper c
 
@@ -44,6 +62,14 @@ Write Arbitrary instances for Uop and Bop.
 (Consider whether it makes sense to implement shrink here.)
 -}
 
+instance Arbitrary Uop where
+  arbitrary :: Gen Uop
+  arbitrary = elements [Neg, Not, Len]
+
+instance Arbitrary Bop where
+  arbitrary :: Gen Bop
+  arbitrary = elements [Plus, Minus, Times, Divide, Eq, Gt, Ge, Lt, Le, Concat]
+
 {-
 Exercise 3
 Write Arbitrary instances for Expression and Statement.
@@ -52,12 +78,57 @@ Your definition of arbitrary for each should use sized.
 Be sure to implement both arbitrary and shrink.
 -}
 
+instance Arbitrary Expression where
+  arbitrary :: Gen Expression
+  arbitrary = sized genExpression
+    where
+      genExpression n | n <= 0 = fmap Val arbitrary
+      genExpression n = frequency [(3, genValue), (2, genUop), (1, genBop)]
+        where
+          genValue = fmap Val arbitrary
+          genUop = fmap Op1 arbitrary <*> genExpression (n - 1)
+          genBop = fmap Op2 (genExpression (n - 1)) <*> arbitrary <*> genExpression (n - 1)
+
+  shrink :: Expression -> [Expression]
+  shrink (Val value) = map Val (shrink value)
+  shrink (Op1 uop exp) = exp : map (Op1 uop) (shrink exp)
+  shrink (Op2 exp1 bop exp2) =
+    exp1 : exp2 : do
+      (exp1s, exp2s) <- shrink (exp1, exp2)
+      return $ Op2 exp1s bop exp2s
+
+instance Arbitrary Statement where
+  arbitrary :: Gen Statement
+  arbitrary = sized genStatement
+    where
+      genStatement n | n <= 0 = fmap Return arbitrary
+      genStatement n = oneof [genIf, genReturn]
+        where
+          genIf = fmap If arbitrary <*> genStatement (n - 1) <*> genStatement (n - 1)
+          genReturn = fmap Return arbitrary
+
+  shrink :: Statement -> [Statement]
+  shrink (Return exp) = fmap Return (shrink exp)
+  shrink (If exp stt1 stt2) =
+    stt1 : stt2 : do
+      (exps, stt1s, stt2s) <- shrink (exp, stt1, stt2)
+      return $ If exps stt1s stt2s
+
 {-
 Exercise 4
 
 Write roundtrip properties to test that your parsers for values, expressions,
 and statements are left inverses of your implementation of show for each.
 -}
+
+prop_roundtrip_value :: Value -> Bool
+prop_roundtrip_value v = parse (parseAll valueP) (show v) == Right (v, "")
+
+prop_roundtrip_expression :: Expression -> Bool
+prop_roundtrip_expression e = parse (parseAll expP) (show e) == Right (e, "")
+
+prop_roundtrip_statement :: Statement -> Bool
+prop_roundtrip_statement s = parse (parseAll statementP) (show s) == Right (s, "")
 
 ---- end of exercises ----
 
@@ -68,10 +139,10 @@ far, not necessarily from this week.
 -}
 
 time :: Double
-time = undefined
+time = 2
 
 question :: String
-question = undefined
+question = "So if property based testing started in Haskell, what are the challenges in setting up property-based tests in other languages? Is it easier? Harder? Better? Less effective?"
 
 check :: Test
 check =
@@ -94,23 +165,80 @@ main = do
 
 -- COPY CODE FROM PREVIOUS WEEKS HERE
 
-instance Functor LuParser
+instance Functor LuParser where
+  fmap :: (a -> b) -> LuParser a -> LuParser b
+  fmap f p = LuParser $ \s -> fmap (\(out, rem) -> (f out, rem)) (parse p s)
 
-instance Applicative LuParser
+instance Applicative LuParser where
+  pure :: a -> LuParser a
+  pure output = LuParser $ \s -> Right (output, s)
 
-instance Monad LuParser
+  (<*>) :: LuParser (a -> b) -> LuParser a -> LuParser b
+  p1 <*> p2 = LuParser $ \s -> case parse p1 s of
+    Left e -> Left e
+    Right (f, s') -> case parse p2 s' of
+      Left e' -> Left e'
+      Right (v, s'') -> Right (f v, s'')
 
-orP :: LuParser a -> LuParser a -> LuParser a
-orP = undefined
+instance Monad LuParser where
+  return :: a -> LuParser a
+  return = pure
+
+  (>>=) :: LuParser a -> (a -> LuParser b) -> LuParser b
+  p >>= func = LuParser $ \s -> case parse p s of
+    Left err_str -> Left err_str
+    Right (result, rem_string) -> parse (func result) rem_string
+
+filterParse :: (Show a) => (a -> Bool) -> LuParser a -> LuParser a
+filterParse func p = LuParser $ \s -> do
+  (out, rem) <- parse p s
+  if func out
+    then return (out, rem)
+    else Left ("Predicate failed on: " ++ show out)
+
+charWhere :: (Char -> Bool) -> LuParser Char
+charWhere func = LuParser $ \s -> case s of
+  [] -> Left "No characters left."
+  (x : xs) -> if func x then Right (x, xs) else Left ("Predicate failed on: " ++ show x)
 
 char :: Char -> LuParser Char
-char = undefined
+char c = charWhere (== c)
+
+string :: String -> LuParser String
+string = mapM char
 
 digit :: LuParser Char
-digit = undefined
+digit = charWhere isDigit
+
+orP :: LuParser a -> LuParser a -> LuParser a
+orP parser1 parser2 = LuParser $ \s -> case parse parser1 s of
+  Left err_str -> parse parser2 s
+  Right output -> Right output
 
 listP :: LuParser a -> LuParser [a]
-listP = undefined
+listP p = orP (neListP p) (pure [])
+  where
+    -- parse a nonempty list of elements matched by p
+    neListP :: LuParser a -> LuParser [a]
+    neListP p' = do
+      out <- p'
+      rem <- listP p'
+      return (out : rem)
+
+eof :: LuParser ()
+eof = LuParser $ \s -> case s of
+  [] -> Right ((), s)
+  x : xs -> Left "Not yet end of file. "
+
+betweenP :: LuParser a -> LuParser b -> LuParser c -> LuParser b
+betweenP p1 p2 p3 = do
+  _ <- p1
+  o2 <- p2
+  _ <- p3
+  return o2
+
+parseAll :: LuParser a -> LuParser a
+parseAll p = betweenP (pure ()) p eof
 
 -- int parser
 
@@ -130,7 +258,15 @@ intP = LuParser $ \s -> case parse (digits `orP` negIntP) s of
       ds <- listP digit
       return $ d : ds
 
--- Lu datatype and parsing definitions
+keywordP :: String -> a -> LuParser a
+keywordP keyword value = fmap (const value) (string keyword)
+
+parens :: LuParser a -> LuParser a
+parens p = betweenP (char '(') p (char ')')
+
+--------------------------
+
+-- LuParseError, LuParaser, and Lu datatypes
 
 type LuParseError = String
 
@@ -143,11 +279,22 @@ data Value
   | StringVal String
   deriving (Eq, Ord)
 
+instance Show Value where
+  show NilVal = "nil"
+  show (IntVal x) = show x
+  show (BoolVal x) = show x
+  show (StringVal x) = show x
+
 data Uop
   = Neg
   | Not
   | Len
-  deriving (Eq, Enum, Bounded)
+  deriving (Eq)
+
+instance Show Uop where
+  show Neg = "-"
+  show Not = "not"
+  show Len = "#"
 
 data Bop
   = Plus
@@ -160,7 +307,19 @@ data Bop
   | Lt
   | Le
   | Concat
-  deriving (Eq, Enum, Bounded)
+  deriving (Eq)
+
+instance Show Bop where
+  show Plus = "+"
+  show Minus = "-"
+  show Times = "*"
+  show Divide = "//"
+  show Gt = ">"
+  show Ge = ">="
+  show Lt = "<"
+  show Le = "<="
+  show Eq = "=="
+  show Concat = ".."
 
 data Expression
   = Val Value
@@ -168,7 +327,90 @@ data Expression
   | Op2 Expression Bop Expression
   deriving (Eq)
 
+instance Show Expression where
+  show (Val value) = show value
+  show (Op1 uop exp) = show uop ++ "(" ++ show exp ++ ")"
+  show (Op2 exp1 bop exp2) = "(" ++ show exp1 ++ ") " ++ show bop ++ " (" ++ show exp2 ++ ")"
+
 data Statement
   = If Expression Statement Statement
   | Return Expression
   deriving (Eq)
+
+instance Show Statement where
+  show (If exp st1 st2) = "if (" ++ show exp ++ ") then (" ++ show st1 ++ ") else (" ++ show st2 ++ ")"
+  show (Return st) = show st
+
+valueP :: LuParser Value
+valueP = intValP `orP` boolValP `orP` stringValP `orP` nilValP
+  where
+    intValP :: LuParser Value
+    intValP = fmap IntVal intP
+
+    boolValP :: LuParser Value
+    boolValP = fmap BoolVal (keywordP "True" True `orP` keywordP "False" False)
+
+    nilValP :: LuParser Value
+    nilValP = keywordP "nil" NilVal
+
+    stringValP :: LuParser Value
+    stringValP = fmap StringVal (betweenP (char '"') (listP (charWhere (/= '"'))) (char '"'))
+
+expP :: LuParser Expression
+expP = valP `orP` op1P `orP` op2P
+  where
+    valP :: LuParser Expression
+    valP = fmap Val valueP
+
+    op1P :: LuParser Expression
+    op1P = do
+      uop <- uopP
+      exp <- parens expP
+      return (Op1 uop exp)
+
+    op2P :: LuParser Expression
+    op2P = do
+      exp1 <- parens expP
+      char ' '
+      bop <- bopP
+      char ' '
+      exp2 <- parens expP
+      return (Op2 exp1 bop exp2)
+
+uopP :: LuParser Uop
+uopP = negP `orP` notP `orP` lenP
+  where
+    negP :: LuParser Uop
+    negP = keywordP "-" Neg
+
+    notP :: LuParser Uop
+    notP = keywordP "not" Not
+
+    lenP :: LuParser Uop
+    lenP = keywordP "#" Len
+
+bopP :: LuParser Bop
+bopP =
+  keywordP ">=" Ge
+    `orP` keywordP "<=" Le
+    `orP` keywordP "==" Eq
+    `orP` keywordP "//" Divide
+    `orP` keywordP ".." Concat
+    `orP` keywordP "+" Plus
+    `orP` keywordP "-" Minus
+    `orP` keywordP "*" Times
+    `orP` keywordP ">" Gt
+    `orP` keywordP "<" Lt
+
+statementP :: LuParser Statement
+statementP = ifP `orP` fmap Return expP
+  where
+    ifP :: LuParser Statement
+    ifP = do
+      string "if "
+      cond <- parens expP
+      string " then "
+      st1 <- parens statementP
+      string " else "
+      st2 <- parens statementP
+      return $ If cond st1 st2
